d9f243f58eb513c58088993439c86327d382241c
Darwin D Wu <wuddarwin@gmail.com>
2017-01-04 21:58:23 -0800

    Updated hello.s with constant

--- gas/hello.s
+++ gas/hello.s
@@ -1,16 +1,22 @@
-        .global _start
-        .text
+        .set SYS_EXIT,  60
+        .set SYS_WRITE, 1
+        .set STDOUT,    1
 
+        .global _start
+
+        .text
 _start:
-        mov $1, %rax            # system call 1 is write
-        mov $1, %rdi            # file handler 1 is stdout
+        mov $SYS_WRITE, %rax            # system call 1 is write
+        mov $STDOUT, %rdi            # file handler 1 is stdout
         mov $message, %rsi      # address of string to output
-        mov $13, %rdx           # number of bytes
+        mov $len, %rdx          # number of bytes
         syscall                 # invoke operating system
 
-        mov $60, %rax           # system call 60 is exit
+        mov $SYS_EXIT, %rax           # system call 60 is exit
         xor %rdi, %rdi          # want return code 0
         syscall
 
+        .data
 message:
         .ascii "Hello world\n"
+        len = . - message
